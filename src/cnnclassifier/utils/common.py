94e89221ac7a192eb2aeb80264fb4bd64616c54a
Divya196 <divyaramesh196@gmail.com>
2023-11-07 23:56:43 +0530

    data ingestion notebook experiment

--- src/cnnclassifier/utils/common.py
+++ src/cnnclassifier/utils/common.py
@@ -13,29 +13,27 @@ import base64
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
-    
-    Args: 
+
+    Args:
         path_to_yaml (str): path like input
 
     Raises:
         ValueError: if yaml file is empty
-        e : empty file
+        e: empty file
 
     Returns:
-        ConfigBox : ConfigBox type
-        
+        ConfigBox: ConfigBox type
     """
-
     try:
-        with open (path_to_yaml) as yaml_file:
+        with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
-    
     except Exception as e:
         raise e
+    
     
 
 @ensure_annotations
